c7588d840fddb3eb3a2306c2f6aaf020e624636c
Crocodile <linxiayoudongfeng@gmail.com>
2024-03-23 15:48:17 +0800

    day01 实现最简mini-react: v2-抽象虚拟节点

--- main.js
+++ main.js
@@ -1,8 +1,22 @@
+/**
+ * 将节点抽象成虚拟节点
+ */
+const appVNode = {
+  type: 'div',
+  props: {
+    id: 'app',
+    children: ['app']
+  }
+};
 const appEl = document.createElement('div');
 appEl.id = 'app';
 const container = document.querySelector('#root');
 container.append(appEl);
 
+const textVNode = {
+  type: 'TEXT_ELEMENT', // 意为文本节点
+  props: { nodeValue: 'app' }
+};
 const textNode = document.createTextNode('');
 textNode.nodeValue = 'app';
 appEl.append(textNode);
