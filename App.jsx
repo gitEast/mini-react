5f81abd400b0df185cd85364e25d422e85542edf
Crocodile <linxiayoudongfeng@gmail.com>
2024-03-23 18:35:40 +0800

    day01 实现最简mini-react: jsx的编译选择修改

--- App.jsx
+++ App.jsx
@@ -1,4 +1,6 @@
-import React from './core/React';
+// js pragma https://stackoverflow.com/questions/14593350/what-exactly-is-a-javascript-pragma
+/**@jsx ZReact.createElement */
+import ZReact from './core/React';
 
 const App = <div id="app">app</div>;
 
